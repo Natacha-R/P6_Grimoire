87503367382fc1caf67e73df6eb1a52b4068f78b
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-10-16 16:02:01 +0200

    dernier commit

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,22 +1,23 @@
 // ************ Le modèle Book permet de définir quelles informations un objet Book contient **************//
 
+// Importation de Mongoose
 const mongoose = require("mongoose");
 
 // Définit chacunes des colonnes de la base de données et leurs types
 const bookSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  year: { type: Number, required: true },
-  genre: { type: String, required: true },
+  userId: { type: String, required: true }, // Quel utilisateur a ajouté un livre
+  title: { type: String, required: true }, // titre du livre
+  author: { type: String, required: true }, // nom de l'auteur du livre
+  imageUrl: { type: String, required: true }, // image du livre
+  year: { type: Number, required: true }, // année de publication du livre
+  genre: { type: String, required: true }, // genre du livre
   ratings: [
     {
       userId: { type: String, required: true },
       grade: { type: Number, required: true },
     },
-  ],
+  ], // tableau qui contient les notes
   averageRating: { type: Number, required: true },
-});
+}); // note moyenne du livre
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model("Book", bookSchema); // Exportation du Modèle book
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,15 @@
 // ************ Le modèle User permet de définir quelles informations un objet User contient **************//
 
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+// Importation des modules
+const mongoose = require("mongoose"); // bibliothèque utilisée pour interagir avec la base de données MongoDB
+const uniqueValidator = require("mongoose-unique-validator"); // plugin Mongoose qui assure que certaines valeurs, soient uniques (mail)
 
 // Définit chacunes des colonnes de la base de données et leurs types
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // adresse mail
+  password: { type: String, required: true }, // mot de passe utilisateur
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator); // ajoute le plugin mongoose-unique-validator au schéma (mail)
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema); // Exportation du modèle User
